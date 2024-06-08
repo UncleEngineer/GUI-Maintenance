d974641ccd7d476acaef0fbf88937cbf1f7dc657
Uncle Engineer <loong.wissawakorn@gmail.com>
2024-06-08 11:53:32 +0700

    เพิ่มตัวแปรช่องกรอก entry

--- basicgui.py
+++ basicgui.py
@@ -5,7 +5,7 @@ GUI = Tk()
 GUI.title('โปรแกรมซ่อมบำรุง by Loong')
 GUI.geometry('500x500+50+50')
 ####FONT#####
-FONT1 = ('Angsana New',20)
+FONT1 = ('Angsana New',20,'bold')
 FONT2 = ('Angsana New',15)
 
 #############
@@ -15,35 +15,43 @@ L.pack()
 #-------------
 L = Label(GUI,text='ชื่อผู้แจ้ง',font=FONT2)
 L.place(x=30,y=50)
-E1 = Entry(GUI,font=FONT2)
+v_name = StringVar() #ตัวแปรพิเศษใช้กับ GUI
+E1 = Entry(GUI,textvariable=v_name, font=FONT2)
 E1.place(x=150,y=50)
 
 #-------------
 L = Label(GUI,text='แผนก',font=FONT2)
 L.place(x=30,y=100)
-E1 = Entry(GUI,font=FONT2)
-E1.place(x=150,y=100)
+v_department =StringVar()
+E2 = Entry(GUI,textvariable=v_department,font=FONT2)
+E2.place(x=150,y=100)
 #-------------
 L = Label(GUI,text='อุปกรณ์/เครื่อง',font=FONT2)
 L.place(x=30,y=150)
-E1 = Entry(GUI,font=FONT2)
-E1.place(x=150,y=150)
+v_machine =StringVar()
+E3 = Entry(GUI,textvariable=v_machine,font=FONT2)
+E3.place(x=150,y=150)
 #-------------
 L = Label(GUI,text='อาการเสีย',font=FONT2)
 L.place(x=30,y=200)
-E1 = Entry(GUI,font=FONT2)
-E1.place(x=150,y=200)
+v_problem =StringVar()
+E4 = Entry(GUI,textvariable=v_problem ,font=FONT2)
+E4.place(x=150,y=200)
 #-------------
 L = Label(GUI,text='หมายเลข',font=FONT2)
 L.place(x=30,y=250)
-E1 = Entry(GUI,font=FONT2)
-E1.place(x=150,y=250)
+v_number =StringVar()
+E5 = Entry(GUI,textvariable=v_number,font=FONT2)
+E5.place(x=150,y=250)
 #-------------
 L = Label(GUI,text='เบอร์โทร',font=FONT2)
 L.place(x=30,y=300)
-E1 = Entry(GUI,font=FONT2)
-E1.place(x=150,y=300)
-# L.pack()
-# L.grid(row=1,column=1)
-# L.place(x=20,y=100)
+v_tel =StringVar()
+E6 = Entry(GUI,textvariable=v_tel,font=FONT2)
+E6.place(x=150,y=300)
+
+B = Button(GUI, text='บันทึกใบแจ้งซ่อม')
+B.place(x=200,y=350)
+
+
 GUI.mainloop()
