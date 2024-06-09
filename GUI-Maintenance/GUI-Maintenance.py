2160888898cd8dfb597aeb005b045c23006146dd
Uncle Engineer <loong.wissawakorn@gmail.com>
2024-06-09 15:05:24 +0700

    update table and insert data from db

--- GUI-Maintenance/GUI-Maintenance.py
+++ GUI-Maintenance/GUI-Maintenance.py
@@ -1,5 +1,7 @@
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk # Theme of TK
+
 # SONGLINE LIB
 import csv
 from datetime import datetime
@@ -14,51 +16,64 @@ def writecsv(record_list):
 GUI = Tk()
 
 GUI.title('โปรแกรมซ่อมบำรุง by Loong')
-GUI.geometry('500x500+50+50')
+GUI.geometry('800x600+50+50')
 ####FONT#####
 FONT1 = ('Angsana New',20,'bold')
 FONT2 = ('Angsana New',15)
 
+#######TAB#######
+Tab = ttk.Notebook(GUI)
+T1 = Frame(Tab)
+T2 = Frame(Tab)
+T3 = Frame(Tab)
+Tab.add(T1,text='ใบแจ้งซ่อม')
+Tab.add(T2,text='ดูใบแจ้งซ่อม')
+Tab.add(T3,text='สรุป')
+Tab.pack(fill=BOTH,expand=1)
+
+
+
+
 #############
-L = Label(GUI,text='ใบแจ้งซ่อม',font=FONT1)
-L.pack()
+L = Label(T1,text='ใบแจ้งซ่อม',font=FONT1)
+L.place(x=80,y=10)
 
 #-------------
-L = Label(GUI,text='ชื่อผู้แจ้ง',font=FONT2)
+L = Label(T1,text='ชื่อผู้แจ้ง',font=FONT2)
 L.place(x=30,y=50)
 v_name = StringVar() #ตัวแปรพิเศษใช้กับ GUI
-E1 = Entry(GUI,textvariable=v_name, font=FONT2)
+E1 = ttk.Entry(T1,textvariable=v_name, font=FONT2)
 E1.place(x=150,y=50)
 
 #-------------
-L = Label(GUI,text='แผนก',font=FONT2)
+L = Label(T1,text='แผนก',font=FONT2)
 L.place(x=30,y=100)
 v_department =StringVar()
-E2 = Entry(GUI,textvariable=v_department,font=FONT2)
+E2 = ttk.Entry(T1,textvariable=v_department,font=FONT2)
 E2.place(x=150,y=100)
 #-------------
-L = Label(GUI,text='อุปกรณ์/เครื่อง',font=FONT2)
+L = Label(T1,text='อุปกรณ์/เครื่อง',font=FONT2)
 L.place(x=30,y=150)
 v_machine =StringVar()
-E3 = Entry(GUI,textvariable=v_machine,font=FONT2)
+E3 = ttk.Entry(T1,textvariable=v_machine,font=FONT2)
 E3.place(x=150,y=150)
 #-------------
-L = Label(GUI,text='อาการเสีย',font=FONT2)
+L = Label(T1,text='อาการเสีย',font=FONT2)
 L.place(x=30,y=200)
 v_problem =StringVar()
-E4 = Entry(GUI,textvariable=v_problem ,font=FONT2)
+E4 = ttk.Entry(T1,textvariable=v_problem ,font=FONT2)
 E4.place(x=150,y=200)
 #-------------
-L = Label(GUI,text='หมายเลข',font=FONT2)
+L = Label(T1,text='หมายเลข',font=FONT2)
 L.place(x=30,y=250)
 v_number =StringVar()
-E5 = Entry(GUI,textvariable=v_number,font=FONT2)
+E5 = ttk.Entry(T1,textvariable=v_number,font=FONT2)
 E5.place(x=150,y=250)
 #-------------
-L = Label(GUI,text='เบอร์โทร',font=FONT2)
+L = Label(T1,text='เบอร์โทร',font=FONT2)
 L.place(x=30,y=300)
 v_tel =StringVar()
-E6 = Entry(GUI,textvariable=v_tel,font=FONT2)
+E6 = ttk.Entry(T1,textvariable=v_tel,font=FONT2)
 E6.place(x=150,y=300)
 
 def save():
@@ -85,16 +100,43 @@ def save():
     v_problem.set('')
     v_number.set('')
     v_tel.set('')
+    update_table()
 
 
     # datalist = [dt,name,department,machine,problem,number,tel]
     # writecsv(datalist)
     # messagebox.showinfo('กำลังบันทึกข้อมูล...',text)    
-
-
-
-B = Button(GUI, text='บันทึกใบแจ้งซ่อม',command=save)
+B = ttk.Button(T1, text='บันทึกใบแจ้งซ่อม',command=save)
 B.place(x=200,y=350)
 
+
+################TAB2################
+header = ['TSID','ชื่อ','แผนก','อุปกรณ์','อาการเสีย','หมายเลข','เบอร์โทรผู้แจ้ง']
+headerw = [50,100,100,150,200,100,100]
+
+mtworkorderlist = ttk.Treeview(T2,columns=header,show='headings',height=10)
+mtworkorderlist.pack()
+
+for h,w in zip(header,headerw):
+    # h='TSID', w=50 ---> h='ชื่อ' w=100
+    mtworkorderlist.heading(h,text=h)
+    mtworkorderlist.column(h,width=w,anchor='center')
+
+mtworkorderlist.column('TSID',anchor='e')
+
+# mtworkorderlist.insert('','end',values=['A','B','C','D','E','F','G'])
+
+def update_table():
+    #clear ข้อมูลเก่า
+    mtworkorderlist.delete(*mtworkorderlist.get_children())
+    data = view_mtworkorder()
+    # print(data)
+    for d in data:
+        d = list(d) #แปลง tuple เป็น list
+        del d[0] # ลบ ID จาก database ออก
+        mtworkorderlist.insert('','end',values=d)
+
+#####START UP######
+update_table()
 
 GUI.mainloop()
